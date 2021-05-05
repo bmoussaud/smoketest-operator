cd337c3b2c5ecdabe1c3381d2422ba2a77da342e
Benoit Moussaud <benoit@moussaud.org>
2021-05-05 17:47:51 +0200

    manage logger

--- python-operator/katapult-operator.py
+++ python-operator/katapult-operator.py
@@ -29,8 +29,8 @@ def k8s_api():
 
 
 @kopf.on.create('smoketests')
-def create_smoketest(body, name, meta, spec, namespace, status, **kwargs):
-    print(f"CREATE create_smoketests")
+def create(body, name, meta, spec, namespace, status, logger, **kwargs):
+    logger.info(f"create smoke test")
     template_job = "templates/job_complete.yaml"
 
     job_name = f"job-smoke-test-{name}"
@@ -43,7 +43,7 @@ def create_smoketest(body, name, meta, spec, namespace, status, **kwargs):
     containers = doc['spec']['template']['spec']['containers']
     container = containers[0]
     container['env'] = env
-    pprint.pprint(doc)
+    logger.info(pprint.pformat(doc))
 
     doc['metadata']['name'] = job_name
     kopf.adopt(doc)
@@ -58,26 +58,25 @@ def create_smoketest(body, name, meta, spec, namespace, status, **kwargs):
 
 
 @kopf.on.event('', 'v1', 'pods', labels={'parent-name': kopf.PRESENT})
-def event_in_a_pod(labels, status, namespace, **kwargs):
-    print(f"event_in_a_pod")
-    print(f"event_in_a_pod {status}")
+def event_in_a_pod(labels, status, namespace, logger, **kwargs):
+    logger.debug(f"event_in_a_pod {status}")
     phase = status.get('phase')
-    print(f"event_in_a_pod {phase}")
+    logger.info(f"event_in_a_pod {phase}")
     query = SmokeTest.objects(k8s_api(), namespace=namespace)
     try:
         parent = query.get_by_name(labels['parent-name'])
-        print(f"event_in_a_pod {parent}")
+        logger.debug(f"event_in_a_pod {parent}")
         parent.patch({'status': {'state': phase}})
     except pykube.ObjectDoesNotExist:
-        print(
-            f" SmokeTest name={labels['parent-name']} does not exist anymore")
+        logger.info(
+            f"associated smokeTest name='{labels['parent-name']}' does not exist anymore in '{namespace}' namespace")
 
 
 @ kopf.on.delete('smoketests')
-def delete_smoketest(body, meta, name, spec, namespace, status, **kwargs):
-    print(f"DELETE create_smoketests")
+def delete_smoketest(body, meta, name, spec, namespace, status, logger, **kwargs):
+    logger.info(f"delete smoke test")
     job_name = f"job-smoke-test-{name}"
-    print(f"job name is {job_name}")
+    logger.info(f"job name is {job_name}")
     query = pykube.Job.objects(k8s_api(), namespace=namespace)
     parent = query.get_by_name(job_name)
     parent.delete(propagation_policy="Background")
@@ -85,8 +84,10 @@ def delete_smoketest(body, meta, name, spec, namespace, status, **kwargs):
 
 def spec_to_env(spec):
     env = []
-    print("spec_to_env")
-    pprint.pprint(spec)
+
+    # pprint.pprint(spec)
+    # logger.info(pprint.pformat(spec))
+
     for key in spec:
         name = stringcase.constcase(key)
         if spec[key] is True:
